259736d2118a6b96b9d091c150c35be1d772a5a0
2Erik <erik19yogo@gmail.com>
2026-05-09 20:54:02 -0500

    ERTV: add Parte 2 ArrayList

---
+++ src/main/java/com/krakedev/examen/entidades/AdminVentas.java
@@ -0,0 +1,38 @@
+package com.krakedev.examen.entidades;
+
+import java.util.ArrayList;
+
+public class AdminVentas {
+	
+	private ArrayList<Vendedor> vendedores;
+
+    // Constructor
+    public AdminVentas() {
+        vendedores = new ArrayList<Vendedor>();
+    }
+
+    // Getter and Setters
+    public ArrayList<Vendedor> getVendedores() {
+        return vendedores;
+    }
+
+    public void setVendedores(ArrayList<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
+    
+    //Metodo agregar
+    public void agregar(Vendedor vendedor) {
+    	vendedores.add(vendedor);
+    }
+    
+    //Metodo calcular sueldo
+    public Double calcularSueldo(String cedula) {
+    	for (Vendedor v : vendedores) {
+			if(v.getCedula().equals(cedula)) {
+				return v.calcularSueldo();
+			}
+		}
+    	return null;
+    }
+	
+}
---
+++ src/test/java/com/krakedev/examen/vendedor/TestAdmin.java
@@ -0,0 +1,36 @@
+package com.krakedev.examen.vendedor;
+
+import com.krakedev.examen.entidades.AdminVentas;
+import com.krakedev.examen.entidades.Vendedor;
+import com.krakedev.examen.entidades.VendedorComision;
+import com.krakedev.examen.entidades.VendedorMixto;
+
+public class TestAdmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AdminVentas admin = new AdminVentas();
+
+		Vendedor vendedor = new Vendedor("111");
+		vendedor.setSueldoFijo(800);
+
+		VendedorComision vendedorComision = new VendedorComision("222");
+		vendedorComision.setNumeroVentas(10);
+		vendedorComision.setComisionPorVenta(50);
+
+		VendedorMixto vendedorMixto = new VendedorMixto("333");
+		vendedorMixto.setSueldoFijo(1000);
+		vendedorMixto.setNumeroVentas(5);
+
+		admin.agregar(vendedor);
+		admin.agregar(vendedorComision);
+		admin.agregar(vendedorMixto);
+
+		System.out.println("Sueldo vendedor: " + admin.calcularSueldo("111"));
+		System.out.println("Sueldo vendedor con comision: " + admin.calcularSueldo("222"));
+		System.out.println("Sueldo vendedor mixto: " + admin.calcularSueldo("333"));
+
+		System.out.println("No existe: " + admin.calcularSueldo("999"));
+	}
+
+}
