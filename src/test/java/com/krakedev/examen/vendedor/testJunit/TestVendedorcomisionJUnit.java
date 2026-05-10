93b620833f39d416d345b4563c57ea7adfbeb062
2Erik <erik19yogo@gmail.com>
2026-05-09 20:30:03 -0500

    ERTV: add class Vendedormixto

---
+++ src/test/java/com/krakedev/examen/vendedor/testJunit/TestVendedorcomisionJUnit.java
@@ -0,0 +1,22 @@
+package com.krakedev.examen.vendedor.testJunit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen.entidades.VendedorComision;
+
+public class TestVendedorcomisionJUnit {
+
+	@Test
+	public void testCalcularSueldo() {
+
+		VendedorComision vendedor = new VendedorComision("1723456789");
+		vendedor.setNumeroVentas(10);
+		vendedor.setComisionPorVenta(50.0);
+
+		double resultado = vendedor.calcularSueldo();
+
+		assertEquals(500.0, resultado);
+	}
+}
