23d18b84b379a7fd7589ee8d8c68dfba2c109ff6
2Erik <erik19yogo@gmail.com>
2026-05-09 20:05:31 -0500

    ERTV: add class TestVendedor

---
+++ src/test/java/com/krakedev/examen/vendedor/TestVendedor.java
@@ -0,0 +1,14 @@
+package com.krakedev.examen.vendedor;
+
+import com.krakedev.examen.entidades.Vendedor;
+
+public class TestVendedor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vendedor vendedor = new Vendedor("111111111");
+		
+		System.out.println(vendedor);
+	}
+
+}
